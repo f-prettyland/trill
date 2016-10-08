338eb11b38c5230ea7a4baf1c65669b228138de3
f-prettyland <f-prettyland@users.noreply.github.com>
2016-10-08 18:41:20 +0100

    Working OO version of stuff

--- check.py
+++ check.py
@@ -4,8 +4,10 @@ import os
 import json
 import string
 import argparse
+from datetime import datetime
 from xml_writer import IncidentXMLWriter
 from survey_handler import SurveyHandler
+from person import Person
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 CATAGORIES="catagories.csv"
@@ -18,19 +20,28 @@ def check_for(checkee, words):
                       for x in words))
   return re.search( regex, checkee)
 
-def identify_str(to_parse):
+def get_initial_text_catagories(to_parse):
   id_catagories = []
   for catagory in svr_handler.catagories.keys():
     if check_for(to_parse, svr_handler.catagories[catagory]):
       id_catagories.append(catagory)
   return id_catagories
 
-# def initial_text(to_parse):
-#   id_catagories = []
-#   for catagory in svr_handler.catagories.keys():
-#     if check_for(to_parse, svr_handler.catagories[catagory]):
-#       id_catagories.append(catagory)
-#   return id_catagories
+def make_person(to_parse, number):
+  print("\nIdentifying: \n", to_parse)
+  print("\nGot these:")
+  id_catagories = get_initial_text_catagories(to_parse)
+  print(id_catagories)
+  print("\n")
+
+  incident_xml = IncidentXMLWriter(to_parse,
+                            id_catagories,
+                            "45.34",
+                            "34.56",
+                            "2016-10-07T20:47:56.000-04:00")
+  incident_xml.printXML()
+
+  return Person(number, id_catagories, datetime.now(), incident_xml)
 
 def main(results):
   global svr_handler
@@ -38,15 +49,10 @@ def main(results):
                           results.lang + "/"+CATAGORIES)
   input_json_loc = os.path.join(__location__,results.input_folder + "/" + \
                           results.lang + "/"+JAAAASON)
-  output_loc = os.path.join(__location__,results.output)
 
   data_file = open(input_json_loc)
   json_data = json.load(data_file)
   svr_handler = SurveyHandler(input_loc, json_data)
-
-  # for catagory in svr_handler.catagories.keys():
-  #   print(catagory)
-  #   print("     ", svr_handler.catagories[catagory])
 
   to_parse = ""
   if results.input:
@@ -54,19 +60,8 @@ def main(results):
   else:
     to_parse = input(ASK_FOR_IN)
 
-  print("\nIdentifying: \n", to_parse)
-  print("\nGot these:")
-  id_catagories = identify_str(to_parse)
-  print(id_catagories)
-  print("\n")
-
-  incident = IncidentXMLWriter(to_parse,
-                              id_catagories,
-                              "45.34",
-                              "34.56",
-                              "2016-10-07T20:47:56.000-04:00")
-  incident.printXML()
-  incident.writeXML(output_loc)
+  p = make_person(to_parse,"0909090909")
+  p.generateXML()
 
 if __name__ == "__main__":
   prsr = argparse.ArgumentParser(description='Identify what someone\'s chatting about')
@@ -77,11 +72,11 @@ if __name__ == "__main__":
                       help='Two letter language representation')
   prsr.add_argument('-i', dest='input',
                       help='A string to parse')
-  prsr.add_argument('-o', dest='output',
-                      help='Where output xml goes to')
+  # prsr.add_argument('-o', dest='output',
+  #                     help='Where output xml goes to')
 
   prsr.set_defaults(input_folder="input")
   prsr.set_defaults(lang="en")
-  prsr.set_defaults(output="out/test.xml")
+  # prsr.set_defaults(output="out/test.xml")
   results = prsr.parse_args()
   main(results)
--- person.py
+++ person.py
@@ -1,3 +1,5 @@
+FILE_LOC = "out/"
+FILE_TYPE = ".xml"
 
 class Person:
   phonenumber = None
@@ -7,22 +9,23 @@ class Person:
   last_time_of_contact = None
   xml = None
 
-  def __init__(self, phonenumber, catagories_matched, current_catagory,
+  def __init__(self, phonenumber, catagories_matched,
               date_time, xml):
+    self.phonenumber = phonenumber
     self.catagories_matched = catagories_matched
     if len(catagories_matched) >= 1 :
-      self.current_catagory = current_catagory[0]
+      self.current_catagory = catagories_matched[0]
     self.current_question = 0
     self.last_time_of_contact = date_time
     self.xml = xml
 
-  def getNextQuestion():
+  # def getNextQuestion():
 
 
-
-  def generateXML():
-    if xml:
+  def generateXML(self):
+    if self.xml:
       # WRITE A LOG
-      print("xml lol")
+      print("WRITE A LOG")
+      self.xml.writeXML(FILE_LOC+self.phonenumber+FILE_TYPE)
     else:
       raise Exception('No XML')
